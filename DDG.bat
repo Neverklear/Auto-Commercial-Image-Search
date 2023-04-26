7d531861f88ea61daf203ccc96fa8a0ada1b242e
Cameron Coppoletta <ccoppoletta@my.devry.edu>
2023-04-26 12:44:29 -0400

    Update DDG.bat

--- DDG.bat
+++ DDG.bat
@@ -7,5 +7,5 @@ set SEARCH_ENGINE_URL=https://duckduckgo.com/?q=
 for /f "tokens=* delims= " %%a in (%INPUT_FILE%) do (
   set query=%%a
   set query=!query: =+!
-  start "" "%SEARCH_ENGINE_URL%!query!&iax=images&ia=images&iaf=license%3AShareCommercially"
+  start "" "%SEARCH_ENGINE_URL%!query!&iax=images&ia=images&iaf=license%%3AShareCommercially"
 )
